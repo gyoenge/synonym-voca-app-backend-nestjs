99866bb284ee33b389799f5708843ec9d288dbe7
gyoengit <gyoengnotion@gmail.com>
2025-01-23 00:58:21 +0900

    add signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,27 @@
 import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import { AuthCredentialDto } from "./dto/auth-credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 export class UserRepository extends Repository<User> {
     constructor(@InjectRepository(User) private dataSource: DataSource) {
         super(User, dataSource.manager)
+    }
+
+    async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+        const {username, password} = authCredentialDto;
+
+        const user = this.create({username, password})
+
+        try {
+            await this.save(user);
+        } catch(error) {
+            if(error.code === '23505') {
+                throw new ConflictException('Existing username')
+            } else {
+                throw new InternalServerErrorException(); 
+            }
+        }
     }
 }
