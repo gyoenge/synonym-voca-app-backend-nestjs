5837f0cb5a7b2c959480211218528031ba898126
gyoengit <gyoengnotion@gmail.com>
2025-01-23 09:56:07 +0900

    add authTest with GetUser decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +19,11 @@ export class AuthController {
     @Post('/signin')
     signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialDto);
+    }
+
+    @Post('authTest')
+    @UseGuards(AuthGuard())
+    authTest(@GetUser() user: User) {
+        console.log('user', user);
     }
 }
