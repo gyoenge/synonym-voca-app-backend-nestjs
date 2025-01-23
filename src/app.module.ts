b8e5f451b3ce76312d52e61e643c2ae1ea598fe3
gyoengit <gyoengnotion@gmail.com>
2025-01-23 10:05:33 +0900

    fix typeorm dependency - add import in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    AuthModule
+  ],
 })
 export class AppModule {}
