248a308f9638dbb97502ff7a0186e9cc8761e234
gyoengit <gyoengnotion@gmail.com>
2025-01-24 23:45:01 +0900

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Synom API Docs')
+    .setDescription('synonym voca app API description')
+    .setVersion('1.0')
+    // .addTag('tag')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
