79b936afe9fb0c1cf0a1277de1c7f9cea01aa8f2
gyoengit <gyoengnotion@gmail.com>
2025-01-25 23:04:02 +0900

    add start logger

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import * as config from "config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // swagger
-  const config = new DocumentBuilder()
+  const swaggerconfig = new DocumentBuilder()
     .setTitle('Synom API Docs')
     .setDescription('synonym voca app API description')
     .setVersion('1.0')
     // .addTag('tag')
     .addBearerAuth() // Bearer Token 인증 추가
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerconfig);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  // port 
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
+
+  await app.listen(port);
+
+  // log
+  Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
