685f036c1c9bb523a624a87cd4898ca0d18bdbd9
gyoengit <gyoengnotion@gmail.com>
2025-01-25 18:50:04 +0900

    fix collection api swagger

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -6,14 +6,17 @@ import { CreateCollectionDto } from './dto/create-collection.dto';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { CollectionStatus } from './collection-status.enum';
 import { CollectionStatusValidationPipe } from './pipes/collection-status-validation.pipe';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @Controller('collection')
 @UseGuards(AuthGuard())
+@ApiBearerAuth()
 export class CollectionController {
     constructor(private collectionService: CollectionService) {}
 
     @Post()
     @UsePipes(ValidationPipe)
+    @ApiOperation({ summary: 'create collection' })
     createCollection(
         @Body() createCollectionDto: CreateCollectionDto 
     ) : Promise<Collection> {
@@ -21,16 +24,23 @@ export class CollectionController {
     }
 
     @Get('/:id')
+    @ApiOperation({ summary: 'get collection by id' })
+    @ApiParam({ name: 'id', description: 'collection id', example: 1})
     getCollectionById(@Param('id') id: number): Promise<Collection> {
         return this.collectionService.getCollectionById(id);
     }
 
     @Delete('/:id')
+    @ApiOperation({ summary: 'delete collection by id' })
+    @ApiParam({ name: 'id', description: 'collection id', example: 1})
     deleteCollection(@Param('id', ParseIntPipe) id: number): Promise<void> {
         return this.collectionService.deleteCollection(id);
     }
 
     @Patch('/:id')
+    @ApiOperation({ summary: 'update collection status by id' })
+    @ApiParam({ name: 'id', description: 'collection id', example: 1})
+    @ApiBody({ description: 'collection status', examples: { example1: {value: { status:'PRIVATE'}}}})
     updateCollectionStatus(
         @Param('id', ParseIntPipe) id: number,
         @Body('status', CollectionStatusValidationPipe) status: CollectionStatus
