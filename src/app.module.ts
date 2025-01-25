b719f62ff1fb82de3191c6c7db0ff2082d762326
gyoengit <gyoengnotion@gmail.com>
2025-01-25 17:40:01 +0900

    fix remove seedmodule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
-import { SeedModule } from './database/seed/seed.module';
 import { CollectionModule } from './collection/collection.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
-    SeedModule,
     CollectionModule
   ],
 })
