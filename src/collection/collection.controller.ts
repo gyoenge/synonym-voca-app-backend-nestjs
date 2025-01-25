424b662522f04aa978ef5c960ee8b49d4b30362b
gyoengit <gyoengnotion@gmail.com>
2025-01-25 17:38:33 +0900

    add createCollection, getCollectionById

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CollectionService } from './collection.service';
+import { Collection } from './collection.entity';
+import { CreateCollectionDto } from './dto/create-collection.dto';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('collection')
-export class CollectionController {}
+@UseGuards(AuthGuard())
+export class CollectionController {
+    constructor(private collectionService: CollectionService) {}
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    createCollection(
+        @Body() createCollectionDto: CreateCollectionDto 
+    ) : Promise<Collection> {
+        return this.collectionService.createCollection(createCollectionDto);
+    }
+
+    @Get('/:id')
+    getCollectionById(@Param('id') id: number): Promise<Collection> {
+        return this.collectionService.getCollectionById(id);
+    }
+}
--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CollectionRepository } from './collection.repository';
+import { CreateCollectionDto } from './dto/create-collection.dto';
+import { Collection } from './collection.entity';
 
 @Injectable()
-export class CollectionService {}
+export class CollectionService {
+    constructor(
+        private collectionRepository: CollectionRepository
+    ) {}
+
+    createCollection(createCollectionDto: CreateCollectionDto): Promise<Collection> {
+        return this.collectionRepository.createCollection(createCollectionDto);
+    }
+
+    async getCollectionById(id: number): Promise<Collection> {
+        const found = await this.collectionRepository.findOne({ where: {collection_id: id} });
+
+        if(!found) {
+            throw new NotFoundException(`Can't find collection with id ${id}`);
+        }
+
+        return found; 
+    }
+} 
