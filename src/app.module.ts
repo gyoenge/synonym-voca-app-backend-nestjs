1fe14d2340a1813aa3af8b81852b96e84715ed55
gyoengit <gyoengnotion@gmail.com>
2025-01-25 23:13:03 +0900

    add database connection logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { CollectionModule } from './collection/collection.module';
 import { WordModule } from './word/word.module';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -13,4 +14,23 @@ import { WordModule } from './word/word.module';
     WordModule
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  constructor(private readonly dataSource: DataSource) {}
+
+  async onModuleInit() {
+    const options = this.dataSource.options;
+
+    if (this.dataSource.isInitialized) {
+      this.logger.log('Database connected successfully!');
+      this.logger.log(`Type: ${options.type}`);
+      // this.logger.log(`Host: ${options.host}`);
+      // this.logger.log(`Port: ${options.port}`);
+      this.logger.log(`Database: ${options.database}`);
+      // this.logger.log(`Username: ${options.username}`);
+    } else {
+      this.logger.error('Failed to connect to the database.');
+    }
+  }
+}
