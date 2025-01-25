7642f3c36bb1dfc420f5ce94fe78d7f19831ed97
gyoengit <gyoengnotion@gmail.com>
2025-01-25 18:19:26 +0900

    add createCollectionDto swagger

--- src/collection/dto/create-collection.dto.ts
+++ src/collection/dto/create-collection.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateCollectionDto {
     @IsNotEmpty()
+    @ApiProperty({
+        description: 'collection title',
+        example: 'example collection'
+    })
     title: string;
 
     @IsNotEmpty()
+    @ApiProperty({
+        description: "collection description",
+        example: 'example collection description'
+    })
     description: string;
 }
