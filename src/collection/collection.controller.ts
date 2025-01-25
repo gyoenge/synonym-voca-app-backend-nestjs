76f53c85375711e42ecb0b9a2f504562f12e576f
gyoengit <gyoengnotion@gmail.com>
2025-01-25 18:28:41 +0900

    add deleteCollection, updateCollectionStatus

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CollectionService } from './collection.service';
 import { Collection } from './collection.entity';
 import { CreateCollectionDto } from './dto/create-collection.dto';
 import { GetUser } from 'src/auth/get-user.decorator';
+import { CollectionStatus } from './collection-status.enum';
+import { CollectionStatusValidationPipe } from './pipes/collection-status-validation.pipe';
 
 @Controller('collection')
 @UseGuards(AuthGuard())
@@ -21,5 +23,18 @@ export class CollectionController {
     @Get('/:id')
     getCollectionById(@Param('id') id: number): Promise<Collection> {
         return this.collectionService.getCollectionById(id);
+    }
+
+    @Delete('/:id')
+    deleteCollection(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.collectionService.deleteCollection(id);
+    }
+
+    @Patch('/:id')
+    updateCollectionStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', CollectionStatusValidationPipe) status: CollectionStatus
+    ): Promise<Collection> {
+        return this.collectionService.updateCollectionStatus(id, status)
     }
 }
--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CollectionRepository } from './collection.repository';
 import { CreateCollectionDto } from './dto/create-collection.dto';
 import { Collection } from './collection.entity';
+import { CollectionStatus } from './collection-status.enum';
 
 @Injectable()
 export class CollectionService {
@@ -22,4 +23,22 @@ export class CollectionService {
 
         return found; 
     }
+
+    async deleteCollection(id: number): Promise<void> {
+        const result = await this.collectionRepository.delete({collection_id: id});
+
+        if(result.affected === 0) {
+            throw new NotFoundException(`Can't find Collection with id ${id}`)
+        }
+    }
+
+    async updateCollectionStatus(id: number, status: CollectionStatus): Promise<Collection> {
+        const collection = await this.getCollectionById(id);
+
+        collection.status = status;
+        await this.collectionRepository.save(collection);
+
+        return collection; 
+    }
+
 } 
