9709518d2c20892077d3ff2616e44de0cf74e0d5
gyoengit <gyoengnotion@gmail.com>
2025-01-25 21:30:44 +0900

    add word repository imports

--- src/word/word.module.ts
+++ src/word/word.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WordController } from './word.controller';
 import { WordService } from './word.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Word } from './word.entity';
+import { WordRepository } from './word.repository';
 
 @Module({
+  imports: [
+      TypeOrmModule.forFeature([Word])
+    ],
   controllers: [WordController],
-  providers: [WordService]
+  providers: [WordService, WordRepository]
 })
 export class WordModule {}
