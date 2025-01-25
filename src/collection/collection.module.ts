d780b6bf510b48a23fb2e76a2aa3ecd0101a6b8a
gyoengit <gyoengnotion@gmail.com>
2025-01-25 17:17:12 +0900

    add collection typeorm imports

--- src/collection/collection.module.ts
+++ src/collection/collection.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CollectionController } from './collection.controller';
 import { CollectionService } from './collection.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Collection } from './collection.entity';
+import { CollectionRepository } from './collection.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Collection])
+  ],
   controllers: [CollectionController],
-  providers: [CollectionService]
+  providers: [CollectionService, CollectionRepository]
 })
 export class CollectionModule {}
