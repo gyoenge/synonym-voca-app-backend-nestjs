e76fe4798c5404d203ed8ebef03040c5f4aae4d1
gyoengit <gyoengnotion@gmail.com>
2025-01-25 16:56:48 +0900

    add collection base

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { SeedModule } from './database/seed/seed.module';
+import { CollectionModule } from './collection/collection.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    AuthModule
+    AuthModule,
+    SeedModule,
+    CollectionModule
   ],
 })
 export class AppModule {}
