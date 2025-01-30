d0e10a515a210811c26b2f9895ab7d7d5af98f74
gyoengit <gyoengnotion@gmail.com>
2025-01-30 20:04:46 +0900

    fix auth response swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './get-user.decorator';
 import { User } from './user.entity';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -14,12 +14,19 @@ export class AuthController {
 
     @Post('/signup')
     @ApiOperation({ summary: 'sign up' })
+    @ApiResponse({ status: 201, description: "sign up succeed"})
+    @ApiResponse({ status: 400, description: "Bad request"})
+    @ApiResponse({ status: 409, description: "Conflict"})
     signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.authService.signUp(authCredentialDto);
     }
 
     @Post('/signin')
     @ApiOperation({ summary: 'sign in' })
+    @ApiResponse({ status: 201, description: "sign in succeed"})
+    @ApiResponse({ status: 400, description: "Bad request"})
+    @ApiResponse({ status: 401, description: "Unauthorized"})
+    @ApiResponse({ status: 404, description: "User not found"})
     signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialDto);
     }
